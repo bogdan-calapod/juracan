070984f369266c00208af5573cb4fcbdc2ef1f58
Bogdan Marius Calapod <bogdan.marius.calapod@gmail.com>
2018-02-10 01:24:55 +0200

    Change port to envvar

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Welcome to 🌀 Juracan!'))
 
-app.listen(80, () => console.log('Listening on 80'))
+app.listen(PORT, () => console.log('Listening on ' + PORT))
